84dba35306085e2ea9c208c6d6995998c2563556
shehab299 <shehab299@outlook.com>
2023-12-30 13:13:50 +0200

    fix trackend line

--- main.asm
+++ main.asm
@@ -58,10 +58,13 @@ MAIN PROC FAR
     MOV BX,VIDEO_MODE_BX
     INT 10H
 
+
+    BEGIN:
     call setBackgroundColor
     
     ;WELCOME PAGE
     call loadLogo
+
     call drawWelcomePage
 
 
@@ -115,6 +118,8 @@ MAIN PROC FAR
     call MAIN_LOOP
 
     CALL ResetKeyboard
+
+    JMP BEGIN
     HLT
 
     CLOSE:
@@ -123,228 +128,228 @@ MAIN PROC FAR
 
 MAIN ENDP
 
-    getInfo PROC
-    ;PLAYER 1 NAME
-        CHECK_PLAYER1_NAME:
-        mov dh, player1_y/CharHeight + 2    ;ROW
-        mov dl, player1_x/CharWidth     ;COLUMN
-        mov bh, 0      ; Page = 0
-        mov bl, BackgroundColor
-        mov ah, 02h    ; BIOS.SetCursorPosition
-        int 10h   
+getInfo PROC
+;PLAYER 1 NAME
+    CHECK_PLAYER1_NAME:
+    mov dh, player1_y/CharHeight + 2    ;ROW
+    mov dl, player1_x/CharWidth     ;COLUMN
+    mov bh, 0      ; Page = 0
+    mov bl, BackgroundColor
+    mov ah, 02h    ; BIOS.SetCursorPosition
+    int 10h   
 
-        mov ah, 0AH
-        mov dx, offset player1_name
-        int 21h
+    mov ah, 0AH
+    mov dx, offset player1_name
+    int 21h
 
-        MOV AL, player1_name + 1
-        CMP AL, 0
-        JNE CHECK_first_character
+    MOV AL, player1_name + 1
+    CMP AL, 0
+    JNE CHECK_first_character
 
-        call emptyNameMsg
-        call clear_input1
-        wait_for_Enterr:
-        MOV AH, 0
-        INT 16H
-        CMP AH, EnterKey
-        JNE wait_for_Enterr
-        call clear_warnings
-        JMP CHECK_PLAYER1_NAME
+    call emptyNameMsg
+    call clear_input1
+    wait_for_Enterr:
+    MOV AH, 0
+    INT 16H
+    CMP AH, EnterKey
+    JNE wait_for_Enterr
+    call clear_warnings
+    JMP CHECK_PLAYER1_NAME
 
 
-        CHECK_first_character:
-        MOV AL, player1_name + 2
-        CMP AL, UPPERCASE_MIN_ASCII
+    CHECK_first_character:
+    MOV AL, player1_name + 2
+    CMP AL, UPPERCASE_MIN_ASCII
+    JB INVALID_NAME
+    CMP AL, LOWERCASE_MAX_ASCII
+    JA INVALID_NAME
+    CMP AL, UPPERCASE_MAX_ASCII
+    JA CHECK_IF_LESS_97
+    JMP CHECK_PLAYER2_name
+    CHECK_IF_LESS_97:
+        CMP AL, LOWERCASE_MIN_ASCII
         JB INVALID_NAME
-        CMP AL, LOWERCASE_MAX_ASCII
-        JA INVALID_NAME
-        CMP AL, UPPERCASE_MAX_ASCII
-        JA CHECK_IF_LESS_97
         JMP CHECK_PLAYER2_name
-        CHECK_IF_LESS_97:
-            CMP AL, LOWERCASE_MIN_ASCII
-            JB INVALID_NAME
-            JMP CHECK_PLAYER2_name
-        
-        INVALID_NAME:
-        call clear_input1
-        Call invalid_Msg
-        wait_for_Enter:
-        MOV AH, 0
-        INT 16H
-        CMP AH, EnterKey
-        JNE wait_for_Enter
-        call clear_warnings
+    
+    INVALID_NAME:
+    call clear_input1
+    Call invalid_Msg
+    wait_for_Enter:
+    MOV AH, 0
+    INT 16H
+    CMP AH, EnterKey
+    JNE wait_for_Enter
+    call clear_warnings
 
-        JMP CHECK_PLAYER1_NAME
+    JMP CHECK_PLAYER1_NAME
 
-        ;PLAYER 2 NAME
-        CHECK_PLAYER2_name:
-        mov dh, player2_y/CharHeight + 2    ;ROW
-        mov dl, player2_x/CharWidth         ;COLUMN
-        mov bh, 0                           ;Page = 0
-        mov bl, BackgroundColor
-        mov ah, 02h                         ;BIOS.SetCursorPosition
-        int 10h
+    ;PLAYER 2 NAME
+    CHECK_PLAYER2_name:
+    mov dh, player2_y/CharHeight + 2    ;ROW
+    mov dl, player2_x/CharWidth         ;COLUMN
+    mov bh, 0                           ;Page = 0
+    mov bl, BackgroundColor
+    mov ah, 02h                         ;BIOS.SetCursorPosition
+    int 10h
 
-        ;take input form the user
-        mov ah, 0AH
-        mov dx, offset player2_name
-        int 21h
+    ;take input form the user
+    mov ah, 0AH
+    mov dx, offset player2_name
+    int 21h
 
-        MOV AL, player2_name + 1
-        CMP AL, 0
-        JNE CHECK_first_character2
+    MOV AL, player2_name + 1
+    CMP AL, 0
+    JNE CHECK_first_character2
 
-        call clear_input2
-        call emptyNameMsg
-        wait_for_Enterr2:
-        MOV AH, 0
-        INT 16H
-        CMP AH, EnterKey
-        JNE wait_for_Enterr2
-        call clear_warnings
-        JMP CHECK_PLAYER2_NAME
+    call clear_input2
+    call emptyNameMsg
+    wait_for_Enterr2:
+    MOV AH, 0
+    INT 16H
+    CMP AH, EnterKey
+    JNE wait_for_Enterr2
+    call clear_warnings
+    JMP CHECK_PLAYER2_NAME
 
-        CHECK_first_character2:
-        MOV AL, player2_name + 2
-        CMP AL, UPPERCASE_MIN_ASCII
+    CHECK_first_character2:
+    MOV AL, player2_name + 2
+    CMP AL, UPPERCASE_MIN_ASCII
+    JB INVALID_NAME2
+    CMP AL, LOWERCASE_MAX_ASCII
+    JA INVALID_NAME2
+    CMP AL, UPPERCASE_MAX_ASCII
+    JA CHECK2_IF_LESS_97
+    JMP EXTI_GET_INFO
+    CHECK2_IF_LESS_97:
+        CMP AL, LOWERCASE_MIN_ASCII
         JB INVALID_NAME2
-        CMP AL, LOWERCASE_MAX_ASCII
-        JA INVALID_NAME2
-        CMP AL, UPPERCASE_MAX_ASCII
-        JA CHECK2_IF_LESS_97
         JMP EXTI_GET_INFO
-        CHECK2_IF_LESS_97:
-            CMP AL, LOWERCASE_MIN_ASCII
-            JB INVALID_NAME2
-            JMP EXTI_GET_INFO
-        
-        INVALID_NAME2:
-        call clear_input2
-        call invalid_Msg
-        wait_for_Enter2:
-        MOV AH, 0
-        INT 16H
-        CMP AH, EnterKey
-        JNE wait_for_Enter2
-        call clear_warnings
-        JMP CHECK_PLAYER2_NAME
+    
+    INVALID_NAME2:
+    call clear_input2
+    call invalid_Msg
+    wait_for_Enter2:
+    MOV AH, 0
+    INT 16H
+    CMP AH, EnterKey
+    JNE wait_for_Enter2
+    call clear_warnings
+    JMP CHECK_PLAYER2_NAME
 
-        
-        EXTI_GET_INFO:
-        RET
-    getInfo ENDP
+    
+    EXTI_GET_INFO:
+    RET
+getInfo ENDP
 
-    ExitProg Proc
-        mov ah,4ch
-        int 21h
-	ExitProg ENDP
+ExitProg Proc
+    mov ah,4ch
+    int 21h
+ExitProg ENDP
 
-    emptyNameMsg PROC
-        MOV DX, offset emptyName_file_name
-        MOV DI, offset file_handle
-        CALL openFile
+emptyNameMsg PROC
+    MOV DX, offset emptyName_file_name
+    MOV DI, offset file_handle
+    CALL openFile
 
-        MOV CX, emptyName_file_size
-        MOV DX, offset emptyName_file
-        MOV BX, [file_handle]
-        CALL ReadFileToMemory
+    MOV CX, emptyName_file_size
+    MOV DX, offset emptyName_file
+    MOV BX, [file_handle]
+    CALL ReadFileToMemory
 
-        MOV SI, offset emptyName_file
-        MOV AX, emptyName_starting_row
-        MOV [START_ROW], AX
-        MOV AX, emptyName_starting_column
-        MOV [START_COLUMN], AX
-        MOV [AUX_IMAGE_HEIGHT], emptyName_height
-        MOV [AUX_IMAGE_WIDTH], emptyName_width
-        CALL drawImage
-        RET
-    emptyNameMsg ENDP
+    MOV SI, offset emptyName_file
+    MOV AX, emptyName_starting_row
+    MOV [START_ROW], AX
+    MOV AX, emptyName_starting_column
+    MOV [START_COLUMN], AX
+    MOV [AUX_IMAGE_HEIGHT], emptyName_height
+    MOV [AUX_IMAGE_WIDTH], emptyName_width
+    CALL drawImage
+    RET
+emptyNameMsg ENDP
 
-    invalid_Msg PROC
-        MOV DX, offset firstCharCheck_file_name
-        MOV DI, offset file_handle
-        CALL openFile
+invalid_Msg PROC
+    MOV DX, offset firstCharCheck_file_name
+    MOV DI, offset file_handle
+    CALL openFile
 
-        MOV CX, firstCharCheck_file_size
-        MOV DX, offset firstCharCheck_file
-        MOV BX, [file_handle]
-        CALL ReadFileToMemory
+    MOV CX, firstCharCheck_file_size
+    MOV DX, offset firstCharCheck_file
+    MOV BX, [file_handle]
+    CALL ReadFileToMemory
 
-        MOV SI, offset firstCharCheck_file
-        MOV AX, firstCharCheck_starting_row
-        MOV [START_ROW], AX
-        MOV AX, firstCharCheck_starting_column
-        MOV [START_COLUMN], AX
-        MOV [AUX_IMAGE_HEIGHT], firstCharCheck_height
-        MOV [AUX_IMAGE_WIDTH], firstCharCheck_width
-        CALL drawImage
-        RET
-    invalid_Msg ENDP
+    MOV SI, offset firstCharCheck_file
+    MOV AX, firstCharCheck_starting_row
+    MOV [START_ROW], AX
+    MOV AX, firstCharCheck_starting_column
+    MOV [START_COLUMN], AX
+    MOV [AUX_IMAGE_HEIGHT], firstCharCheck_height
+    MOV [AUX_IMAGE_WIDTH], firstCharCheck_width
+    CALL drawImage
+    RET
+invalid_Msg ENDP
 
-    clear_input1 PROC
-        mov cx, 64                ;Column
-        mov dx, 290               ;Row
-        mov al, BackgroundColor   ;Pixel color
-        mov ah, 0ch               ;Draw Pixel Command
-        clear_text1: int 10h
-        INC CX
-        MOV BX, CX
-        SUB BX, 182
-        CMP BX, 66
-        JNZ clear_text1
+clear_input1 PROC
+    mov cx, 64                ;Column
+    mov dx, 290               ;Row
+    mov al, BackgroundColor   ;Pixel color
+    mov ah, 0ch               ;Draw Pixel Command
+    clear_text1: int 10h
+    INC CX
+    MOV BX, CX
+    SUB BX, 182
+    CMP BX, 66
+    JNZ clear_text1
 
-        MOV CX, 64
-        INC DX
-        MOV BX, DX
-        SUB BX, 32
-        CMP BX, 290
-        JNZ clear_text1
-        RET
-    clear_input1 ENDP
+    MOV CX, 64
+    INC DX
+    MOV BX, DX
+    SUB BX, 32
+    CMP BX, 290
+    JNZ clear_text1
+    RET
+clear_input1 ENDP
 
-    clear_input2 PROC
-        mov cx, 367                 ;Column
-        mov dx, 290                 ;Row
-        mov al, BackgroundColor     ;Pixel color
-        mov ah, 0ch                 ;Draw Pixel Command
-        clear_text2: int 10h
-        INC CX
-        MOV BX, CX
-        SUB BX, 182
-        CMP BX, 367
-        JNZ clear_text2
+clear_input2 PROC
+    mov cx, 367                 ;Column
+    mov dx, 290                 ;Row
+    mov al, BackgroundColor     ;Pixel color
+    mov ah, 0ch                 ;Draw Pixel Command
+    clear_text2: int 10h
+    INC CX
+    MOV BX, CX
+    SUB BX, 182
+    CMP BX, 367
+    JNZ clear_text2
 
-        MOV CX, 367
-        INC DX
-        MOV BX, DX
-        SUB BX, 32
-        CMP BX, 290
-        JNZ clear_text2
-        RET
-    clear_input2 ENDP
+    MOV CX, 367
+    INC DX
+    MOV BX, DX
+    SUB BX, 32
+    CMP BX, 290
+    JNZ clear_text2
+    RET
+clear_input2 ENDP
 
-    clear_warnings PROC
-        mov cx, emptyName_starting_column        ;Column
-        mov dx, emptyName_starting_row           ;Row
-        mov al, BackgroundColor                  ;Pixel color
-        mov ah, 0ch                              ;Draw Pixel Command
-        clear_msgs: int 10h
-        INC CX
-        MOV BX, CX
-        SUB BX, emptyName_width
-        CMP BX, emptyName_starting_column + 2
-        JNZ clear_msgs
+clear_warnings PROC
+    mov cx, emptyName_starting_column        ;Column
+    mov dx, emptyName_starting_row           ;Row
+    mov al, BackgroundColor                  ;Pixel color
+    mov ah, 0ch                              ;Draw Pixel Command
+    clear_msgs: int 10h
+    INC CX
+    MOV BX, CX
+    SUB BX, emptyName_width
+    CMP BX, emptyName_starting_column + 2
+    JNZ clear_msgs
 
-        MOV CX, emptyName_starting_column
-        INC DX
-        MOV BX, DX
-        SUB BX, emptyName_height
-        CMP BX, emptyName_starting_row + 2
-        JNZ clear_msgs
-        RET
-    clear_warnings ENDP
+    MOV CX, emptyName_starting_column
+    INC DX
+    MOV BX, DX
+    SUB BX, emptyName_height
+    CMP BX, emptyName_starting_row + 2
+    JNZ clear_msgs
+    RET
+clear_warnings ENDP
 
 findCarRemSteps proc ;DI -> X_POS ; SI -> Y_POS
     MOV DX,0
@@ -382,6 +387,58 @@ findCarRemSteps proc ;DI -> X_POS ; SI -> Y_POS
     jl search_findcarindex
     ret
 findCarRemSteps endp
+
+; LoadCarsForPlayers proc
+
+;     ;PLAYER 1
+
+;     ;FIRST CAR
+;     CMP player1_carIndex, 0
+;     JNE SECOND_CAR
+;     CALL loadFlameCarForOne
+
+;     SECOND_CAR:
+;     CMP player1_carIndex, 1
+;     JNE THIRD_CAR
+;     CALL loadRedCarForOne
+
+
+;     THIRD_CAR:
+;     CMP player1_carIndex, 2
+;     JNE FOURTH_CAR
+;     CALL loadBlueCarForOne
+
+;     FOURTH_CAR:
+;     CMP player1_carIndex, 3
+;     JNE PLAYER2_CAR
+;     CALL loadYellowCarForOne
+
+;     PLAYER2_CAR:
+
+;     ;FIRST CAR
+;     CMP player2_carIndex, 0
+;     JNE SECOND_CAR_2
+;     CALL loadFlameCarForTwo
+
+;     SECOND_CAR_2:
+;     CMP player2_carIndex, 1
+;     JNE THIRD_CAR_2
+;     CALL loadRedCarForTwo
+
+;     THIRD_CAR:
+;     CMP player1_carIndex, 2
+;     JNE FOURTH_CAR_2
+;     CALL loadBlueCarForTwo
+
+;     FOURTH_CAR:
+;     CMP player1_carIndex, 3
+;     JNE END_CAR
+;     CALL loadYellowCarForTwo
+
+;     END_CAR:
+;     RET
+
+; LoadCarsForPlayers ENDP
 
 
 END MAIN
